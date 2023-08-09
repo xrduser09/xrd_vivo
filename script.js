6c55ab5b2b09c757a7f71ffd6e9a0f99e4d3db1f
XRDSahil <141648403+XRDSahil@users.noreply.github.com>
2023-08-09 14:41:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -126,118 +126,113 @@
 
 
 
+// // Round Chart JS
+// document.addEventListener("DOMContentLoaded", function() {
 
+//   let vivoPercentage=50;     // Example value, replace with actual value
+//   var oppoPercentage = 65;      // Example value, replace with actual value
+//   var samsungPercentage = 35;   // Example value, replace with actual value
+//   var xiaomiPercentage = 60;    // Example value, replace with actual value
 
+//   var circleProgress = (function(selector) {
+//     var wrapper = document.querySelectorAll(selector);
+//     Array.prototype.forEach.call(wrapper, function(wrapper, i) {
+//       var wrapperWidth,
+//         wrapperHeight,
+//         percent,
+//         innerHTML,
+//         context,
+//         lineWidth,
+//         centerX,
+//         centerY,
+//         radius,
+//         newPercent,
+//         speed,
+//         from,
+//         to,
+//         duration,
+//         start,
+//         strokeStyle,
+//         text;
 
+//       var getValues = function() {
+//         wrapperWidth = parseInt(window.getComputedStyle(wrapper).width);
+//         wrapperHeight = wrapperWidth;
+//         percent = parseInt(wrapper.getAttribute('data-cp-percentage')) || 0;
+//         innerHTML = '<span class="percentage"><strong>' + percent + '</strong> %</span><canvas class="circleProgressCanvas" width="' + (wrapperWidth * 2) + '" height="' + wrapperHeight * 2 + '"></canvas>';
+//         wrapper.innerHTML = innerHTML;
+//         text = wrapper.querySelector(".percentage");
+//         canvas = wrapper.querySelector(".circleProgressCanvas");
+//         wrapper.style.height = canvas.style.width = canvas.style.height = wrapperWidth + "px";
+//         context = canvas.getContext('2d');
+//         centerX = canvas.width / 2;
+//         centerY = canvas.height / 2;
+//         newPercent = 0;
+//         speed = 1;
+//         from = 0;
+//         to = percent;
+//         duration = 1000;
+//         lineWidth = 25;
+//         radius = canvas.width / 2 - lineWidth;
+//         strokeStyle = wrapper.getAttribute('data-cp-color');
+//         start = new Date().getTime();
+//       };
 
+//       function animate() {
+//         requestAnimationFrame(animate);
+//         var time = new Date().getTime() - start;
+//         if (time <= duration) {
+//           var x = easeInOutQuart(time, from, to - from, duration);
+//           newPercent = x;
+//           text.innerHTML = Math.round(newPercent) + " %";
+//           drawArc();
+//         }
+//       }
 
-// Round Chart JS
-document.addEventListener("DOMContentLoaded", function() {
+//       function drawArc() {
+//         var circleStart = 1.5 * Math.PI;
+//         var circleEnd = circleStart + (newPercent / 50) * Math.PI;
+//         context.clearRect(0, 0, canvas.width, canvas.height);
+//         context.beginPath();
+//         context.arc(centerX, centerY, radius, circleStart, 4 * Math.PI, false);
+//         context.lineWidth = lineWidth;
+//         context.strokeStyle = "#ddd";
+//         context.stroke();
+//         context.beginPath();
+//         context.arc(centerX, centerY, radius, circleStart, circleEnd, false);
+//         context.lineWidth = lineWidth;
+//         context.strokeStyle = strokeStyle;
+//         context.stroke();
+//       }
 
-  var vivoPercentage = 50;      // Example value, replace with actual value
-  var oppoPercentage = 65;      // Example value, replace with actual value
-  var samsungPercentage = 35;   // Example value, replace with actual value
-  var xiaomiPercentage = 60;    // Example value, replace with actual value
+//       var update = function() {
+//         getValues();
+//         var counters = document.querySelectorAll(".counter");
+// counters[0].setAttribute("data-cp-percentage", vivoPercentage);
+// counters[1].setAttribute("data-cp-percentage", oppoPercentage);
+// counters[2].setAttribute("data-cp-percentage", samsungPercentage);
+// counters[3].setAttribute("data-cp-percentage", xiaomiPercentage);
+//         animate();
+//       };
+//       update();
 
-  var circleProgress = (function(selector) {
-    var wrapper = document.querySelectorAll(selector);
-    Array.prototype.forEach.call(wrapper, function(wrapper, i) {
-      var wrapperWidth,
-        wrapperHeight,
-        percent,
-        innerHTML,
-        context,
-        lineWidth,
-        centerX,
-        centerY,
-        radius,
-        newPercent,
-        speed,
-        from,
-        to,
-        duration,
-        start,
-        strokeStyle,
-        text;
+//       var resizeTimer;
+//       window.addEventListener("resize", function() {
+//         clearTimeout(resizeTimer);
+//         resizeTimer = setTimeout(function() {
+//           clearTimeout(resizeTimer);
+//           start = new Date().getTime();
+//           update();
+//         }, 250);
+//       });
+//     });
 
-      var getValues = function() {
-        wrapperWidth = parseInt(window.getComputedStyle(wrapper).width);
-        wrapperHeight = wrapperWidth;
-        percent = parseInt(wrapper.getAttribute('data-cp-percentage')) || 0;
-        innerHTML = '<span class="percentage"><strong>' + percent + '</strong> %</span><canvas class="circleProgressCanvas" width="' + (wrapperWidth * 2) + '" height="' + wrapperHeight * 2 + '"></canvas>';
-        wrapper.innerHTML = innerHTML;
-        text = wrapper.querySelector(".percentage");
-        canvas = wrapper.querySelector(".circleProgressCanvas");
-        wrapper.style.height = canvas.style.width = canvas.style.height = wrapperWidth + "px";
-        context = canvas.getContext('2d');
-        centerX = canvas.width / 2;
-        centerY = canvas.height / 2;
-        newPercent = 0;
-        speed = 1;
-        from = 0;
-        to = percent;
-        duration = 1000;
-        lineWidth = 25;
-        radius = canvas.width / 2 - lineWidth;
-        strokeStyle = wrapper.getAttribute('data-cp-color');
-        start = new Date().getTime();
-      };
+//     function easeInOutQuart(t, b, c, d) {
+//       if ((t /= d / 2) < 1) return c / 2 * t * t * t * t + b;
+//       return -c / 2 * ((t -= 2) * t * t * t - 2) + b;
+//     }
 
-      function animate() {
-        requestAnimationFrame(animate);
-        var time = new Date().getTime() - start;
-        if (time <= duration) {
-          var x = easeInOutQuart(time, from, to - from, duration);
-          newPercent = x;
-          text.innerHTML = Math.round(newPercent) + " %";
-          drawArc();
-        }
-      }
+//   });
 
-      function drawArc() {
-        var circleStart = 1.5 * Math.PI;
-        var circleEnd = circleStart + (newPercent / 50) * Math.PI;
-        context.clearRect(0, 0, canvas.width, canvas.height);
-        context.beginPath();
-        context.arc(centerX, centerY, radius, circleStart, 4 * Math.PI, false);
-        context.lineWidth = lineWidth;
-        context.strokeStyle = "#ddd";
-        context.stroke();
-        context.beginPath();
-        context.arc(centerX, centerY, radius, circleStart, circleEnd, false);
-        context.lineWidth = lineWidth;
-        context.strokeStyle = strokeStyle;
-        context.stroke();
-      }
-
-      var update = function() {
-        getValues();
-        var counters = document.querySelectorAll(".counter");
-counters[0].setAttribute("data-cp-percentage", vivoPercentage);
-counters[1].setAttribute("data-cp-percentage", oppoPercentage);
-counters[2].setAttribute("data-cp-percentage", samsungPercentage);
-counters[3].setAttribute("data-cp-percentage", xiaomiPercentage);
-        animate();
-      };
-      update();
-
-      var resizeTimer;
-      window.addEventListener("resize", function() {
-        clearTimeout(resizeTimer);
-        resizeTimer = setTimeout(function() {
-          clearTimeout(resizeTimer);
-          start = new Date().getTime();
-          update();
-        }, 250);
-      });
-    });
-
-    function easeInOutQuart(t, b, c, d) {
-      if ((t /= d / 2) < 1) return c / 2 * t * t * t * t + b;
-      return -c / 2 * ((t -= 2) * t * t * t - 2) + b;
-    }
-
-  });
-
-  circleProgress('.counter');
-});
+//   circleProgress('.counter');
+// });
